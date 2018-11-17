8353f8917701861097c2e81b5a1202cf1cb0d752
Rahul Kumar <rforrahul83@gmail.com>
2018-11-17 15:22:53 -0500

    writting and testing with mocha

---
+++ test/demo_test.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+// describe tests
+describe('some test', () => {
+
+    // create tests
+    it('adds two numbers', () => {
+        assert(2 + 4 === 6);
+    });
+
+});
